0e5da93859ebccd7f85ca59a8ab862dbb6a97f9e
= <=>
2017-12-06 12:56:13 +0000

    Close burger on link navigation

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,7 @@
     
     <div class="navbar-brand">
       <router-link class="navbar-item" to="/">
-        <strong>QuizHome</strong>
+        <strong @click="burgerOpen=false">QuizHome</strong>
       </router-link>
 
       <button class="button navbar-burger" :class="{'is-active': burgerOpen}" @click="toggleBurger()">
@@ -16,19 +16,19 @@
     <div class="navbar-menu" :class="{'is-active': burgerOpen}">
       <div class="navbar-end">
         <router-link ref="signUpButton" v-if="!loggedIn" class="navbar-item" to="/signup">
-          <strong>Create Account</strong>
+          <strong @click="toggleBurger()">Create Account</strong>
         </router-link>
         <router-link ref="logInButton" v-if="!loggedIn" class="navbar-item" to="/signin">
-          <strong>Login</strong>
+          <strong @click="toggleBurger()">Login</strong>
         </router-link>
         <router-link ref="createQuizButton" v-if="loggedIn" class="navbar-item" to="/quizmaker">
-          <strong>Make a Quiz</strong>
+          <strong @click="toggleBurger()">Make a Quiz</strong>
         </router-link>
         <router-link ref="profileButton" v-if="loggedIn" class="navbar-item" to="/profile">
-          <strong>Profile</strong>
+          <strong @click="toggleBurger()">Profile</strong>
         </router-link>
-        <a ref="logOutButton" id="logout" v-if="loggedIn" @click="signOut" class="navbar-item">
-          <strong>logout</strong>
+        <a ref="logOutButton" id="logout" v-if="loggedIn" @click="signOut()" class="navbar-item">
+          <strong @click="toggleBurger()">logout</strong>
         </a>
       </div>
     </div>
@@ -51,6 +51,7 @@ export default {
       this.$emit('logout')
     },
     toggleBurger() {
+      console.log("toggle")
       if(this.burgerOpen){ this.burgerOpen = false }
       else{ this.burgerOpen = true }
     }
